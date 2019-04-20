724a80a473187808b6cf4890de09676822985105
Jiajun <jiajun.xiong@eigen.capital>
2019-04-20 14:26:50 +0800

    WIP

--- client/base_client.py
+++
@@ -1,9 +0,0 @@
-class BaseClient(object):
-
-    def __init__(self, api_key: str, api_secret: str):
-        NotImplementedError
-
-    def get_latest_trade_time(self, symbol):
-        NotImplementedError
-
-    def get_
